b8b72a650d5c97d146f0896c498b5d7146ed71d6
qf19910623 <qf19910623@gmail.com>
2020-02-14 18:16:03 +0800

    配置白名单

---
+++ config-sample.py
@@ -0,0 +1,16 @@
+db = {
+    'default':  {
+        'host': '127.0.0.1',
+        'port': '3306',
+        'dbuser': 'root',
+        'dbpass': '123456',
+        'dbname': 'papai',
+    }
+}
+redis = {
+    'default':  {
+        'host': '127.0.0.1',
+        'port': '6379',
+        'password': '',
+    }
+}
