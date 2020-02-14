09b89f72ce4d221c1891fa42d474aef668bd8cb1
qf19910623 <qf19910623@gmail.com>
2020-02-14 12:12:54 +0800

    清理配置

--- config.py
+++ config.py
@@ -14,37 +14,3 @@ redis = {
         'password': '',
     }
 }
-tencent = {
-    'idcard':  {
-        'secretid': 'AKID96rMeho9uQiqjRvCI8C3f6esstjMjFZi',
-        'secretkey': '3toZ7RmtlSv9EA0f8LNJ6i6MOYXHoQr5'
-    },
-    'invoice':  {
-        'secretid': 'AKID96rMeho9uQiqjRvCI8C3f6esstjMjFZi',
-        'secretkey': '3toZ7RmtlSv9EA0f8LNJ6i6MOYXHoQr5'
-    },
-    'general':  {
-        'secretid': 'AKID96rMeho9uQiqjRvCI8C3f6esstjMjFZi',
-        'secretkey': '3toZ7RmtlSv9EA0f8LNJ6i6MOYXHoQr5'
-    }
-}
-faceid = {
-    'idcard':  {
-        'api_key': 'rlN7N8CiT77yls3RuxrqtKGx7rv6JW66',
-        'api_secret': '5OagY6r_PQYdtX3tMczjPCkAm4dneVkq'
-    }
-}
-xunfei = {
-    'idcard':  {
-        'appid': '5e08308b',
-        'api_key': '1f6e4f4e92619b27c704be5e413d7718'
-    },
-    'invoice':  {
-        'appid': '5e08308b',
-        'api_key': '1f6e4f4e92619b27c704be5e413d7718'
-    },
-    'general':  {
-        'appid': '5e08308b',
-        'api_key': '1f6e4f4e92619b27c704be5e413d7718'
-    }
-}
