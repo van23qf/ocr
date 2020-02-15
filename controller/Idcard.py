b8b9d94bd0ec5312d7c200d7a49ecb54f619134c
qf19910623 <qf19910623@gmail.com>
2020-02-15 12:33:21 +0800

    文件参数修改

--- controller/Idcard.py
+++ controller/Idcard.py
@@ -12,15 +12,15 @@ from system import global_dict
 def index():
     side = request.form.get('side')
     project = request.form.get('project')
-    file_name = request.form.get('file_name')
+    file = base64.b64decode(request.form.get('file'))
     api_provider = request.form.get('api_provider')
     api_config = func.get_api_config('idcard', project, api_provider)
     if not api_config:
         raise Exception('配置错误')
     global_dict.set_value("api_config", api_config)
-    file = func.read_file(file_name)
-    if not file:
-        raise Exception('文件读取失败')
+    # file = func.read_file(file_name)
+    # if not file:
+    #     raise Exception('文件读取失败')
     if api_provider == 'faceid':
         result = FaceidIdcardOCR.ocr(file, side)
     elif api_provider == 'tencent':
--- controller/Liveness.py
+++ controller/Liveness.py
@@ -14,15 +14,15 @@ def url():
     idcard_name = request.form.get('idcard_name')
     idcard_number = request.form.get('idcard_number')
     project = request.form.get('project')
-    file_name = request.form.get('file_name')
+    file = base64.b64decode(request.form.get('file'))
     api_provider = request.form.get('api_provider')
     api_config = func.get_api_config('liveness', project, api_provider)
     if not api_config:
         raise Exception('配置错误')
     global_dict.set_value("api_config", api_config)
-    file = func.read_file(file_name)
-    if not file:
-        raise Exception('文件读取失败')
+    # file = func.read_file(file_name)
+    # if not file:
+    #     raise Exception('文件读取失败')
     if api_provider == 'faceid':
         result = FaceidLiveness.url(idcard_name, idcard_number, file)
     else:
